567eebafcbf3dd3a5e8a8c546570ab1fd752de77
Jimmique <jimmique88@gmail.com>
2019-11-08 19:15:14 -0800

    Completed Answers section, yarn install, refactored App.js to a class component, added data fet and set to state, added player components, passed state data and rendered.

---
+++ client/src/components/Player.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Player({ name, country, searches }) {
+    return (
+      <div>
+        <h3>{name}</h3>
+        <p>{country}</p>
+        <p>{searches}</p>
+      </div>
+    );
+
+ }
+
+ export default Player;
---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+ import Player from './Player';
+
+ function PlayerList({ players }) {
+   return (
+    players.map( player => (
+        <Player 
+          key={player.id} 
+          name={player.name} 
+          country={player.country}
+          searches={player.searches}
+        />
+      )) 
+   );
+ }
+
+ export default PlayerList;
